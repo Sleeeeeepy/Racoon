66a4d7b50e2575b5e1d0dd64cf0345bfe8893112
Sleeeeeepy <adjustedmin@gmail.com>
2023-08-23 01:22:38 +0900

    Move SerializationHelper from Racoon.Core.Packet to Racoon.Core.Util.

---
+++ Racoon.Core/Util/SerializationHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Racoon.Core.Packet;
+
+namespace Racoon.Core.Util;
+
+public static class SerializationHelper
+{
+    public static bool Serialize(byte[] buffer, PacketBase header, IPacket body)
+    {
+        if (body is not ISerializable)
+        {
+            return false;
+        }
+
+        int packetLength = header.Length + body.Length;
+        if (buffer.Length < packetLength)
+        {
+            return false;
+        }
+
+        bool result = true;
+        result &= header.Serialize(buffer, 0);
+        result &= ((ISerializable)body).Serialize(buffer, PacketBase.HeaderSize);
+
+        return result;
+    }
+
+    public static bool Deserialize<T>(byte[] buffer, PacketBase header, T body) where T : IDeserializable<T>
+    {
+        if (buffer.Length < PacketBase.HeaderSize)
+        {
+            return false;
+        }
+
+        int startIndex = 0;
+        int endIndex = PacketBase.HeaderSize;
+
+        bool result = true;
+        result &= PacketBase.Deserialize(buffer, header) != null;
+
+        int packetLength = PacketBase.HeaderSize + header.Length;
+        if (buffer.Length < packetLength)
+        {
+            return false;
+        }
+
+        startIndex = endIndex;
+        endIndex = startIndex + header.Length;
+        result &= T.Deserialize(buffer.AsSpan()[startIndex..endIndex], body) != null;
+
+        return result;
+    }
+}
