0c95333f670cff5e4a36d5287c56669526a44f7c
Sleeeeeepy <adjustedmin@gmail.com>
2023-08-23 01:50:14 +0900

    Add new projects - TestConsoleClient and TestConsoleServer

---
+++ Racoon.TestConsoleClient/Program.cs
@@ -0,0 +1,7 @@
+﻿using Racoon.Core.Net;
+
+Console.WriteLine("=== Server ===");
+var client = new RacoonClientSocket("127.0.0.1", 5000);
+Console.WriteLine("Press any key to continue...");
+Console.ReadLine();
+client.Connect();
---
+++ Racoon.TestConsoleServer/Program.cs
@@ -0,0 +1,7 @@
+﻿using Racoon.Core;
+using Racoon.Core.Net;
+
+var server = new RacoonServerSocket("0.0.0.0", 5000);
+Console.WriteLine("Press any key to continue...");
+Console.ReadLine();
+server.Listen();
